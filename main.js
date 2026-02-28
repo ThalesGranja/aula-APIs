2e52f94b132157fbc9c6e8c536c55f385cbfd83d
Thales Granja <thalesgranja.dev@gmail.com>
2026-02-28 02:04:53 -0300

    feat: utilizando o fetch com async await

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-fetch("http://localhost:3333/products").then((response) => response.json()).then((data) => console.log(data));
+// Utilizando o fetch com o then.
+// fetch("http://localhost:3333/products").then((response) => response.json()).then((data) => console.log(data));
 
+// Utilizando o fetch com o async/await
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products");
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchProducts();
